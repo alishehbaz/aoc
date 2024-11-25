f956b2d4eaaa2fd33842b68007827bb6a528ed80
Ali Shehbaz <alishahbaz072@gmail.com>
2024-11-25 00:28:32 -0500

    add(2015): day2p1&p2

---
+++ 2015/day2/p1.py
@@ -0,0 +1,20 @@
+# Ref: https://adventofcode.com/2015/day/3
+
+def part1():
+
+    totalWrappingPaper = 0
+    smallestSide = float('inf')
+
+    with open("input.txt", "r") as file:
+        boxDimensions = file.readlines()
+        for eachBoxDimension in boxDimensions:
+            l, w, h = eachBoxDimension.strip('\n').split('x')
+            l, w, h = int(l), int(w), int(h)
+            smallestSide = min(l*w, w*h, h*l)
+            totalWrappingPaper += 2 * (l*w + w*h + h*l) + smallestSide
+
+    return totalWrappingPaper
+
+
+ans = part1()
+print(f'total wrapping paper needed in sq ft: {ans}')
---
+++ 2015/day2/p2.py
@@ -0,0 +1,28 @@
+# Ref: https://adventofcode.com/2015/day/3
+
+def part2():
+
+    totalRibbonPaper = 0
+
+    with open("input.txt", "r") as file:
+        boxDimensions = file.readlines()
+        for eachBoxDimension in boxDimensions:
+            l, w, h = eachBoxDimension.strip('\n').split('x')
+            l, w, h = int(l), int(w), int(h)
+            smallestSidePerimeter = 2 * \
+                (smallestSides(l, w, h)[0] + smallestSides(l, w, h)[1])
+            totalRibbonPaper += smallestSidePerimeter + l*w*h
+
+    return totalRibbonPaper
+
+
+def smallestSides(l, w, h):
+
+    dimensions = [l, w, h]
+    dimensions.sort()
+
+    return dimensions[0], dimensions[1]
+
+
+ans = part2()
+print(f'total wrapping paper needed for the ribbon in sq ft: {ans}')
