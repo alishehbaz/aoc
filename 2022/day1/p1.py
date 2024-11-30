00e2e5b7ea713fba37c69902eed2385cbf0eb344
Ali Shehbaz <alishahbaz072@gmail.com>
2024-11-30 15:26:55 -0500

    mv(2022): day1p1

---
+++ 2022/day1/p1.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+
+def p1():
+
+    with open('input.txt') as f:
+        lines = [line.strip() for line in f]
+
+    calorieMap = defaultdict(int)
+    elf = 1
+    for eachEntry in lines:
+        if eachEntry == '':
+            elf += 1
+            continue
+        calorieMap[elf] += int(eachEntry)
+
+    return max(calorieMap.values())
+
+
+ans = p1()
+print(f'The highest number of calories carried by Elf : {ans}')
