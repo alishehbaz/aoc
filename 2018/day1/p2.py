e902588b93a75fc1d2895ad17f24225616fa1e2f
Ali Shehbaz <alishahbaz072@gmail.com>
2025-01-11 23:59:53 -0500

    add(2018): day1p2

---
+++ 2018/day1/p2.py
@@ -0,0 +1,26 @@
+# Ref: https://adventofcode.com/2018/day/2
+
+def p2():
+    res = 0
+    with open('input.txt') as f:
+        lines = [int(line.strip()) for line in f]
+
+    duplicateNotFound = True
+    currFreq = 0
+    hashset = set()
+    hashset.add(currFreq)
+
+    while duplicateNotFound:
+        for i in range(len(lines)):
+            currFreq += lines[i]
+            if currFreq not in hashset:
+                hashset.add(currFreq)
+            else:
+                duplicateNotFound = False
+                break
+
+    return res
+
+
+ans = p2()
+print(f'first frequency that is reached twice is {ans}')
