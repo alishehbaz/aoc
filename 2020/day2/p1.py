3a553e99ad04b207a6447f23c3fca8c6b0dea102
Ali Shehbaz <alishahbaz072@gmail.com>
2025-01-21 00:06:08 -0500

    refactor(2020): day2p1 - make it more pythonic

--- 2020/day2/p1.py
+++ 2020/day2/p1.py
@@ -1,33 +1,28 @@
 # Ref: https://adventofcode.com/2020/day/2
 
+
 def p1():
     
-    with open('./input.txt') as f:
+    with open("./input.txt") as f:
         passwordPolicies = [line.strip() for line in f]
-        
+
     validPasswords = 0
-        
-    
+
     for policy in passwordPolicies:
         rule, char, password = policy.split()
-        
+
         if isValid(rule, char, password):
-            print("valid")
-            validPasswords +=1
-            
+            validPasswords += 1
+
     return validPasswords
-        
+
+
 def isValid(rule, char, password):
     
-    minFreq, maxFreq = rule.split('-')
+    minFreq, maxFreq = rule.split("-")
     char = char[0]
-    
-    if password.count(char) > int(minFreq) and password.count(char) < int(maxFreq):
-        return True
-    
-    return False
-    
-    
+
+    return int(minFreq) <= password.count(char) <= int(maxFreq)
 
 ans = p1()
-print(f'ans = {ans}')
+print(f"valid passwords = {ans}")
