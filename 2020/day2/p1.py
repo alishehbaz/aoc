138f69067949abd9aa3af6d842403e401af1b73d
Ali Shehbaz <alishahbaz072@gmail.com>
2025-01-20 23:59:30 -0500

    add(2020): day2p1 & move day1

---
+++ 2020/day2/p1.py
@@ -0,0 +1,33 @@
+# Ref: https://adventofcode.com/2020/day/2
+
+def p1():
+    
+    with open('./input.txt') as f:
+        passwordPolicies = [line.strip() for line in f]
+        
+    validPasswords = 0
+        
+    
+    for policy in passwordPolicies:
+        rule, char, password = policy.split()
+        
+        if isValid(rule, char, password):
+            print("valid")
+            validPasswords +=1
+            
+    return validPasswords
+        
+def isValid(rule, char, password):
+    
+    minFreq, maxFreq = rule.split('-')
+    char = char[0]
+    
+    if password.count(char) > int(minFreq) and password.count(char) < int(maxFreq):
+        return True
+    
+    return False
+    
+    
+
+ans = p1()
+print(f'ans = {ans}')
