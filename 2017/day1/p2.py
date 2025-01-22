acd5381a1f67d2b7e061854d8f7db1ac3bc8323e
Ali Shehbaz <alishahbaz072@gmail.com>
2025-01-21 20:48:28 -0500

    add(2017): day1p2

---
+++ 2017/day1/p2.py
@@ -0,0 +1,22 @@
+def p1():
+    
+    with open("./input.txt") as f:
+        for line in f:
+            captcha = line.strip()
+
+    return getSum(captcha)
+
+
+def getSum(captcha):
+    
+    _sum = 0
+
+    for i in range(len(captcha)//2):
+        if captcha[i] == captcha[i+(len(captcha)//2)]:
+            _sum += int(captcha[i])*2
+            
+    return _sum
+
+
+ans = p1()
+print(f"solution to the captcha = {ans}")
