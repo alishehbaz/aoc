eade861dabeecb940f665b063160e789ae1265db
Ali Shehbaz <alishahbaz072@gmail.com>
2025-02-02 23:59:55 -0500

    add(2022): day5p2 - inc

---
+++ 2022/day5/p2.py
@@ -0,0 +1,56 @@
+# Ref: https://adventofcode.com/2022/day/5
+
+from parse import parseInput
+
+
+def p1():
+
+    instructions = [line.strip() for line in open("./input.txt")]
+
+    crateMoverType = 'CrateMover 90001'
+
+    moveInstructions, stackPileMap = parseInput(instructions)
+
+    # print(moveInstructions, stackPileMap)
+
+    for instruction in moveInstructions:
+
+        instruction = instruction.split(' ')
+        boxesToMove, fromStack, toStack = int(instruction[1]), int(
+            instruction[3]), int(instruction[5])
+
+        boxesToMove = getBoxesToBeMoved(stackPileMap, fromStack, boxesToMove)
+        addBoxesToStack(stackPileMap, toStack, boxesToMove, crateMoverType)
+
+    return getTopElements(stackPileMap)
+
+
+def getBoxesToBeMoved(stackPileMap, fromStack, k):
+    popped = []
+    while k:
+        box = stackPileMap[fromStack].pop()
+        popped.append(box)
+        k -= 1
+
+    return popped
+
+
+def addBoxesToStack(stackPileMap, toStack, toAdd, crateMoverType):
+    if crateMoverType == 'CrateMover 9000':
+        stackPileMap[toStack].extend(toAdd)
+    if crateMoverType == 'CrateMover 90001':
+        stackPileMap[toStack].extend(toAdd[::-1])
+
+
+def getTopElements(stackPileMap):
+
+    topBoxesString = ''
+
+    for key, value in stackPileMap.items():
+        topBoxesString += value[-1]
+
+    return topBoxesString
+
+
+ans = p1()
+print(f'ans = {ans}')
