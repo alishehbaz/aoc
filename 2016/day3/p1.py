d4195be9412ef167af330027943e088b9da32ca8
Ali Shehbaz <alishahbaz072@gmail.com>
2025-02-07 23:59:40 -0500

    add(2016): day3p1

---
+++ 2016/day3/p1.py
@@ -0,0 +1,17 @@
+def p1():
+    
+    triangles = [triangle.strip() for triangle in open("./input.txt")]
+
+    validTriangles = 0
+
+    for triangle in triangles:
+        s1, s2, s3 = triangle.split()
+
+        if int(s1) + int(s2) > int(s3):
+            validTriangles += 1
+
+    return validTriangles
+
+
+ans = p1()
+print(f"valid triangles = {ans}")
