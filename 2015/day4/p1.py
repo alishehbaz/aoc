07199c7aa8edc829c3cd8bb1a80c9bcf88ef3b53
Ali Shehbaz <alishahbaz072@gmail.com>
2025-02-10 23:09:37 -0500

    add(2015): day4p1

---
+++ 2015/day4/p1.py
@@ -0,0 +1,20 @@
+# Ref: https://adventofcode.com/2015/day/4
+
+import hashlib
+
+def p1():
+    
+    puzzleInput = "ckczppom"
+    
+    for i in range(1000000):
+        
+        toBeHashed = puzzleInput+str(i)
+        
+        md5Hash = hashlib.md5(toBeHashed.encode("utf-8")).hexdigest()
+        
+        if md5Hash[:5] == "00000":
+            return i
+
+
+ans = p1()
+print(f'lowest number (when appended to the puzzle string) to produce a hash starting with five zeroes = {ans}')
