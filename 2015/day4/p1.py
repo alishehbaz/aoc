e6dad00387a72fc13d1076587bcb2e5c8ea6bf84
Ali Shehbaz <alishahbaz072@gmail.com>
2025-02-10 23:10:49 -0500

    chore: fix formatting

--- 2015/day4/p1.py
+++ 2015/day4/p1.py
@@ -2,19 +2,20 @@
 
 import hashlib
 
+
 def p1():
-    
     puzzleInput = "ckczppom"
-    
+
     for i in range(1000000):
-        
-        toBeHashed = puzzleInput+str(i)
-        
+        toBeHashed = puzzleInput + str(i)
+
         md5Hash = hashlib.md5(toBeHashed.encode("utf-8")).hexdigest()
-        
+
         if md5Hash[:5] == "00000":
             return i
 
 
 ans = p1()
-print(f'lowest number (when appended to the puzzle string) to produce a hash starting with five zeroes = {ans}')
+print(
+    f"lowest number (when appended to the puzzle string) to produce a hash starting with five zeroes = {ans}"
+)
