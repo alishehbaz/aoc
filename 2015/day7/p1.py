ee84a75b02b72424bfa7cd1943a2c7b01572b528
Ali Shehbaz <alishahbaz072@gmail.com>
2025-02-18 01:14:00 -0500

    add(2015): day7p1 incomplete

--- 2015/day7/p1.py
+++ 2015/day7/p1.py
@@ -1,31 +1,59 @@
+from collections import defaultdict
+# INCOMPLETE
+
+
 def p1():
 
     instructions = [ins.rstrip().split('->') for ins in open("./input.txt")]
 
-    variableMap = {}
+    variableMap = defaultdict(int)
 
     for i in range(len(instructions)):
         for j in range(len(instructions[i])):
             instructions[i][j] = instructions[i][j].strip()
 
-    print(instructions)
+    # print(instructions)
 
-    for instruction in instructions:
+    for ip, instruction in enumerate(instructions):
+        print(f'instruction pointer: {ip}')
+        print(f'instruction: {instruction}')
+
+        dst = instruction[-1]
 
         numOrOp = instruction[0].split(" ")
         if (len(numOrOp) == 1):
-            print(numOrOp)
-            variableMap[instruction[1]] = int(numOrOp[0])
-            print(variableMap)
+            if numOrOp[0].isdigit():
+                variableMap[dst] = int(numOrOp[0])
+            else:
+                variableMap[dst] = variableMap[numOrOp[0]]
 
         if len(numOrOp) == 2:
             if numOrOp[0] == 'NOT':
                 source = numOrOp[1]
                 print(instruction)
-                dst = instruction[1]
-                print(f'dst : {dst}')
-                variableMap[dst] = ~variableMap[source]
-                print(variableMap)
+                variableMap[dst] = ~variableMap[source] & 0xFFFF
+
+        if len(numOrOp) > 2:
+            print(numOrOp)
+            if numOrOp[1] == 'AND':
+                variableMap[dst] = variableMap[numOrOp[0]
+                                               ] & variableMap[numOrOp[2]]
+
+            if numOrOp[1] == 'OR':
+                variableMap[dst] = variableMap[numOrOp[0]
+                                               ] | variableMap[numOrOp[2]]
+
+            if numOrOp[1] == 'LSHIFT':
+                variableMap[dst] = variableMap[numOrOp[0]
+                                               ] << int(numOrOp[2])
+
+            if numOrOp[1] == 'RSHIFT':
+                variableMap[dst] = variableMap[numOrOp[0]
+                                               ] >> int(numOrOp[2])
+
+    print(variableMap)
+
+    print(f"value in {variableMap['a']}")
 
 
 p1()
